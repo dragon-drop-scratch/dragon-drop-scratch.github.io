add9c6d7927072fe600cc1b605996913440b86f4
csf30816 <elijahporter@zoho.com>
2016-08-07 14:28:59 -0500

    Update scratcher.php

--- scratcher.php
+++ scratcher.php
@@ -1,27 +1,26 @@
 <?php
-$servername = "sql202.byethost3.com";
-$username = "b3_18601113";
-$password = "csf30816";
-$dbname = "b3_18601113_signup";
+include_once(dirname(__FILE__) . "/cdynconfig.php");
+include_once(dirname(__FILE__) . "/cdyn_includes/db.php");
+include_once(dirname(__FILE__) ."/cdyn_includes/phputil.php");
+include_once(dirname(__FILE__) ."/cdyn_includes/printform.php");
+$pageName="formpage.php";
+$datastorage="insert";$error="";
+$arr["E1textbox_name"]=(isset($_POST["E1textbox_name"]) && trim($_POST["E1textbox_name"])!="")?trim($_POST["E1textbox_name"]):"";
+$arr["E2textbox_pass"]=(isset($_POST["E2textbox_pass"]) && trim($_POST["E2textbox_pass"])!="")?trim($_POST["E2textbox_pass"]):"";
+$arr["E3radiooptions_reason"]=(isset($_POST["E3radiooptions_reason"]) && trim($_POST["E3radiooptions_reason"])!="")?trim($_POST["E3radiooptions_reason"]):"";
+if($error=="") {
 
-$name=$_POST['scratchname'];
-$pass=$_POST['password'];
-$reason=$_POST['reason'];
+$print_emailobj=new CDYNprintEmailFormData();
+$print_emailobj->textbox("E1textbox_name","Scratch Username:");
+$print_emailobj->textbox("E2textbox_pass","Password");
+$print_emailobj->radioChoice("E3radiooptions_reason","What will you be using the site for?");
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO s_users (s_name, pass, why)
-    VALUES ('$scratchname', '$password', '$reason')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "New record created successfully";
-    }
-catch(PDOException $e)
-    {
-    echo "User could not be created :(";
-    }
+$cdyn_emaildata=$print_emailobj->printFormData();
+/* //Print Form data
+echo $cdyn_emaildata;*/
 
-$conn = null;
-?>
+if($datastorage=="insert"){
+if($db->insert($arr,"formstore165"))
+echo "Data Inserted"; }
+}
+else {include_once($pageName);} ?>
