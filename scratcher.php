f2bc20d80fda5f12636511d909a04f04c2b745dd
csf30816 <elijahporter@zoho.com>
2016-08-07 12:39:41 -0500

    Update scratcher.php
    Don't know if this works yet....

--- scratcher.php
+++ scratcher.php
@@ -1,37 +1,27 @@
 <?php
+$servername = "sql202.byethost3.com";
+$username = "b3_18601113";
+$password = "csf30816";
+$dbname = "b3_18601113_signup";
 
-$host="sql202.byethost3.com"; // Host name 
-$username="b3_18601113"; // Mysql username 
-$password="csf30816"; // Mysql password 
-$db_name="b3_18601113_signup"; // Database name 
-$tbl_name="s_users"; // Table name 
-
-// Connect to server and select database.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
-
-// Get values from form 
 $name=$_POST['scratchname'];
 $pass=$_POST['password'];
 $reason=$_POST['reason'];
 
-// Insert data into mysql 
-$sql="INSERT INTO $tbl_name(s_name, pass, why)VALUES('$scratchname', '$pass', '$reason')";
-$result=mysql_query($sql);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO s_users (s_name, pass, why)
+    VALUES ('$scratchname', '$password', '$reason')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "User could not be created :(";
+    }
 
-// if successfully insert data into database, displays message "Successful". 
-if($result){
-echo "Your account has been created";
-echo "<BR>";
-echo "<a href='./'>Back to main page</a>";
-}
-
-else {
-echo "ERROR";
-}
-?> 
-
-<?php 
-// close connection 
-mysql_close();
+$conn = null;
 ?>
