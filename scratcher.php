1c35fe35d0a3e39311b9153dfec0857e904d611c
csf30816 <elijahporter@zoho.com>
2016-08-08 14:43:51 -0500

    Update scratcher.php

--- scratcher.php
+++ scratcher.php
@@ -8,22 +8,29 @@ $pass=$_POST["pass"];
 $type=$_POST["reason"];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$result = $conn->query("SELECT id FROM scratch WHERE user = '$name'");
+
 // Check connection
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
+
 if ($pass != $name) {
-	$sql = "INSERT INTO scratch (user, pass, type)
+	if ($result->num_rows == 0) {
+		$sql = "INSERT INTO scratch (user, pass, type)
 VALUES ('$name', '$pass', '$type')";
-	if ($conn->query($sql) === TRUE) {
-		echo "The account <b>" . $name . "</b> has been created with the password <b>" . $pass . "</b>. Have a nice day!";
-		echo "<br><a href='./'>Go Home?</a>";
+		if ($conn->query($sql) === TRUE) {
+			echo "The account <b>" . $name . "</b> has been created with the password <b>" . $pass . "</b>. Have a nice day!";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
 	} else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
+	echo "Sorry, but that user already exists";
+	echo "<br><a href='./'>Go Home?</a>";
 	}
 } else {
 	echo "Sorry, but your username and password can not be the same";
-		echo "<br><a href='./'>Go Home?</a>";
+	echo "<br><a href='./'>Go Home?</a>";
 }
 $conn->close();
 ?>
