d51f301acee4a9df55147250632164d6cd4602fc
csf30816 <elijahporter@zoho.com>
2016-08-08 11:44:21 -0500

    Delete config.php

--- php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysql_connect('b3_18601113_signup', 'b3_18601113', 'csf30816');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-?>
