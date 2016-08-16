7a33ca694b3b1fe3a6a8ea4aaa87696b8512dfd7
csf30816 <elijahporter@zoho.com>
2016-08-16 11:17:47 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -8,7 +8,8 @@ $pass=$_POST["pass"];
 $reason=$_POST["reason"];
 $fname=$_POST["fname"];
 $email=$_POST["email"];
-
+$email2=$_POST["email2"];
+$pass2=$_POST["pass2"];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 $result = $conn->query("SELECT id FROM scratch WHERE user = '$name'");
@@ -17,48 +18,56 @@ $userexists = $conn->query("SELECT id FROM users WHERE username = '$name'");
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-if ($pass != 'password') {
-	if (strlen($name) >= 3) {
-		if (strlen($pass) >= 6) {
-			if ($name != '') { //Makes sure the name is not empty
-				if ($pass != '') { //Makes sure the password is not empty
-					if ($pass != $name) { //makes sure the username is not the password
-						if ($result->num_rows and $userexists->num_rows == 0) { //Makes sure the user does not already exist
-							$sql = "INSERT INTO users (username, password, fullname, email, reason)
+if ($email == $email2) {
+	if ($pass === $pass2) {
+		if ($pass != 'password') {
+			if (strlen($name) >= 3) {
+				if (strlen($pass) >= 6) {
+					if ($name != '') { //Makes sure the name is not empty
+						if ($pass != '') { //Makes sure the password is not empty
+							if ($pass != $name) { //makes sure the username is not the password
+								if ($result->num_rows and $userexists->num_rows == 0) { //Makes sure the user does not already exist
+									$sql = "INSERT INTO users (username, password, fullname, email, reason)
 VALUES ('$name', '$pass', '$fname', '$email', '$reason')";
-							if ($conn->query($sql) === TRUE) {
-								echo "The account <b>" . $name . "</b> has been created with the password length of <b>" . strlen($pass) . "</b>. Have a nice day!";
-								echo "<br>You will recieve an email with your information shortly.";
+									if ($conn->query($sql) === TRUE) {
+										echo "The account <b>" . $name . "</b> has been created with the password length of <b>" . strlen($pass) . "</b>. Have a nice day!";
+										echo "<br>You will recieve an email with your information shortly.";
+									} else {
+										echo "Error: " . $sql . "<br>" . $conn->error;
+									}
+								} else {
+									echo "Sorry, but that user already exists";
+									echo "<br><a href='./'>Go Home?</a>";
+								}
 							} else {
-								echo "Error: " . $sql . "<br>" . $conn->error;
+								echo "Sorry, but your username and password can not be the same";
+								echo "<br><a href='./'>Go Home?</a>";
 							}
 						} else {
-							echo "Sorry, but that user already exists";
+							echo "Sorry, but your password can not be blank";
 							echo "<br><a href='./'>Go Home?</a>";
 						}
 					} else {
-						echo "Sorry, but your username and password can not be the same";
+						echo "Sorry, but your password can not be blank";
 						echo "<br><a href='./'>Go Home?</a>";
 					}
 				} else {
-					echo "Sorry, but your password can not be blank";
+					echo "Sorry, but your password can not be shorter than 6 chars";
 					echo "<br><a href='./'>Go Home?</a>";
 				}
 			} else {
-				echo "Sorry, but your password can not be blank";
+				echo "Sorry, but your username can not be shorter than 3 chars";
 				echo "<br><a href='./'>Go Home?</a>";
 			}
 		} else {
-			echo "Sorry, but your password can not be shorter than 6 chars";
+			echo "Really, 'password' as your password? Are you trying to get hacked?";
 			echo "<br><a href='./'>Go Home?</a>";
 		}
 	} else {
-		echo "Sorry, but your username can not be shorter than 3 chars";
-		echo "<br><a href='./'>Go Home?</a>";
+		echo "Passwords do not match";
 	}
 } else {
-	echo "Really, 'password' as your password? Are you trying to get hacked?";
-	echo "<br><a href='./'>Go Home?</a>";
+	echo "Emails do not match";
 }
 $conn->close();
 ?>
