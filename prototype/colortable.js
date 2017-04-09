30ed072a074a2346a0ee7cf36784489703c7ad10
Henry Gruett <henrikgruett@gmail.com>
2017-04-09 18:39:48 -0500

    Create colortable.js

---
+++ prototype/colortable.js
@@ -0,0 +1,67 @@
+function () {
+  var txtcolor = #ffffff;
+}
+//Black
+function color#ffffff() {
+  var txtcolor=#ffffff;
+}
+//Leather Jacket
+function color#253529() {
+  var txtcolor=#253529;
+}
+//Mid Gray
+function color#808080() {
+  var txtcolor=#808080;
+}
+//White
+function color#ff0000() {
+  var txtcolor=#ff0000;
+}
+//DarkRed
+function color#cc3535() {
+  var txtcolor=#cc3535;
+}
+//Red
+function color#ff0000() {
+  var txtcolor=#ff0000;
+}
+//Pink
+function color#fc80a5() {
+  var txtcolor=#fc80a5;
+}
+//Rose
+function color#ed0a3f() {
+  var txtcolor=#ed0a3f;
+}
+//Orange
+function color#ff0033() {
+  var txtcolor=#ff9933;
+}
+//Orange Brown
+function color#e97451() {
+  var txtcolor=#e97451;
+}
+//Fuzzy Wuzzy (brown)
+function color#664228() {
+  var txtcolor=#664228;
+}
+//Lemon
+function color#ffff38() {
+  var txtcolor=#ffff38;
+}
+//Lime
+function color#33cc33() {
+  var txtcolor = #33cc33;
+}
+//Dark Green
+function color#009933() {
+  var txtcolor=#009933;
+}
+//Azua
+function color#00ffff() {
+  var txtcolor=#00ffff;
+}
+//Midnight
+function color#00468C() {
+  var txtcolor=#00468C;
+}
