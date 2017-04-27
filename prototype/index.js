58f2d5c98fcdbd76c27bd54e14d5908aedcf517d
Nanomotion <Nanomotion@users.noreply.github.com>
2017-04-27 08:28:54 -0400

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -43,19 +43,19 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     console.log("The data is: " + data);
-    if (data == "title") {
-        numofe = numofe + 1;
-        numoft = numoft + 1;
-        getSelectionText()
-        document.getElementById(ev.target.id).innerHTML = y + "<div class='element-wrapper'><div class='settings-icon' onClick='settingsDialog(\"title\", \"" + numofe + "\")'></div><div class='text-edit" + numoft + "' id='" + numofe + "' style='width: 75%;border: 1px solid rgb(187, 187, 187);border-radius: 2px;background-color: rgb(224, 224, 224);padding: 5px;border-top-right-radius: 10px;border-top-left-radius: 10px;text-align: center;margin-bottom: 5px;'><button onclick=textadd('i',numoft)><i>Italic</i></button><button onclick=textadd('b',numoft)><b>Bold</b></button><button onclick=textadd('u',numoft)><u>Underline</u></button><button onclick=textadd('s',numoft)><s>Strikethrough</s></button><button onclick=textadd('span',numoft)>Plain</button></div><div id='dd-text-edit" + numofe + "' class='outline-tt' contenteditable>This is a text box <i>Enter text here...</i></div></div>" + "<br>";
-        exportcode = exportcode + "<div style='font-size: 36px;' id='dd-text" + numoft + "'><i></i></div>" + "<br>";
-    }
     if (data == "text") {
         numofe = numofe + 1;
         numoft = numoft + 1;
         getSelectionText()
+        document.getElementById(ev.target.id).innerHTML = y + "<div class='element-wrapper'><div class='settings-icon' onClick='settingsDialog(\"title\", \"" + numofe + "\")'></div><div class='text-edit" + numoft + "' id='" + numofe + "' style='width: 75%;border: 1px solid rgb(187, 187, 187);border-radius: 2px;background-color: rgb(224, 224, 224);padding: 5px;border-top-right-radius: 10px;border-top-left-radius: 10px;text-align: center;margin-bottom: 5px;'><button onclick=textadd('i',numoft)><i>Italic</i></button><button onclick=textadd('b',numoft)><b>Bold</b></button><button onclick=textadd('u',numoft)><u>Underline</u></button><button onclick=textadd('s',numoft)><s>Strikethrough</s></button><button onclick=textadd('span',numoft)>Plain</button></div><div id='dd-text-edit" + numofe + "' class='outline-tt' contenteditable>This is a text box <i>Enter text here...</i></div></div>" + "<br>";
+        exportcode = exportcode + "<div id='dd-text" + numoft + "'><i></i></div>" + "<br>";
+    }
+    if (data == "title") {
+        numofe = numofe + 1;
+        numoft = numoft + 1;
+        getSelectionText()
         document.getElementById(ev.target.id).innerHTML = y + "<div class='element-wrapper'><div class='settings-icon' onClick='settingsDialog(\"text\", \"" + numofe + "\")'></div><div class='text-edit" + numoft + "' id='" + numofe + "' style='width: 75%;border: 1px solid rgb(187, 187, 187);border-radius: 2px;background-color: rgb(224, 224, 224);padding: 5px;border-top-right-radius: 10px;border-top-left-radius: 10px;text-align: center;margin-bottom: 5px;'><button onclick=textadd('i',numoft)><i>Italic</i></button><button onclick=textadd('b',numoft)><b>Bold</b></button><button onclick=textadd('u',numoft)><u>Underline</u></button><button onclick=textadd('s',numoft)><s>Strikethrough</s></button><button onclick=textadd('span',numoft)>Plain</button></div><div id='dd-text-edit" + numofe + "' class='outline-tt' style='font-size: 36px;' contenteditable>Title</div></div>" + "<br>";
-        exportcode = exportcode + "<div  id='dd-text" + numoft + "'>This is a text box. <i>Enter text here...</i></div>" + "<br>";
+        exportcode = exportcode + "<div  style='font-size: 36px;' id='dd-text" + numoft + "'>Title</div>" + "<br>";
     }
     if (data == "photo") {
         swal({
